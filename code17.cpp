1217f60a964a615e015edb1aa39df6beb179e5f4
Vickers <vickerybasket@gmail.com>
2025-05-26 13:17:30 +0530

    Add files via upload

--- code17.cpp
+++ code17.cpp
@@ -5,6 +5,41 @@ class A {
 public:
     void princi() {
         cout << "peak of hierarchey" << endl;
+        cout<<"im a part of this ladder"<<endl;
+    }
+};        
+class B : public A {
+public:
+    void P_HOD(){
+        cout<<"physics"<<endl;
+        princi();
+    }
+};
+class C : public A {
+public:
+    void C_HOD(){
+        cout<<"chemistry"<<endl;
+        princi();
+    };
+};
+
+
+int main() {
+    B obj1;
+    obj1.P_HOD();
+    C obj2;
+    obj2.C_HOD();
+    return 0;
+}
+
+
+/*#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void princi() {
+        cout << "peak of hierarchey" << endl;
     }
    };        
 class B : public A {
@@ -28,3 +63,4 @@ int main() {
     obj2.C_HOD();
     return 0;
 }
+*/
